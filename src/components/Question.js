c52ec69eaf537cdf35eac1d6860ecf8602556531
Rene Funcion <rcfun959@gmail.com>
2023-06-08 10:36:50 -0700

    Completed react-hooks-use-effect-lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    // Start timer when the component is rendered and when theres a new question.
+    const timerId = setTimeout(() => {
+      // If time has run out, user has answered incorrectly and reset the timer.
+      if ((timeRemaining = 1)) {
+        onAnswered(false);
+        setTimeRemaining(10);
+      } else {
+        // Otherwise, just decrement the remaining time.
+        setTimeRemaining(timeRemaining - 1);
+      }
+    }, 1000);
+
+    // Return cleanup function.
+    return () => clearTimeout(timerId);
+  }, [question, timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
